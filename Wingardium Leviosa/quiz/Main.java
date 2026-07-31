4b3581f8be72e7811d248e76329ad3cfb4dc828b
Cyber0Specter <Cyber0Specter@gmail.com>
2026-07-31 20:48:20 +0600

    Hello

---
+++ Wingardium Leviosa/quiz/Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main (String[] args) {
+        int answer=40;
+    System.out.println("Question is what is 25 + 15");
+    if (answer==40){
+        System.out.println("Right");
+    }
+    else{
+        System.out.println("Wrong");
+    }
+
+     int answer2=144;
+    System.out.println("Question 12 squar = ?");
+    if (answer2==144){
+        System.out.println("Right");
+    }
+    else{
+        System.out.println("Wrong");
+    }
+
+    }
+}
